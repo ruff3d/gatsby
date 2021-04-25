931770db5faa08f386e4a3d4e7e4a731794a7f5f
Denys Dudarev <jump2me@gmail.com>
2021-04-25 20:33:42 +0200

    Update date.ts

--- api/date.ts
+++ api/date.ts
@@ -2,5 +2,5 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default (_req: VercelRequest, res: VercelResponse) => {
   const date = new Date().toString();
-  res.status(200).send(date);
+  res.status(200).send(_req.toString());
 };
